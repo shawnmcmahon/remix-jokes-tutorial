bbf2f9945b4483334173869edf156e7ca5ef98b2
shawnmcmahon <shawnmcmahon17@gmail.com>
2022-02-13 11:43:28 -0700

    Create form to submit new joke

--- my-remix-app/app/routes/jokes/new.tsx
+++ my-remix-app/app/routes/jokes/new.tsx
@@ -1,3 +1,22 @@
+import { ActionFunction } from "remix";
+import { redirect } from 'remix';
+import { db } from "~/utils/db.server";
+
+export let action: ActionFunction = async ({request}) => {
+  let form = await request.formData();
+  let name = form.get('name'); 
+  let content = form.get('get');
+  if (typeof name !== 'string' || typeof content !== 'string') {
+    throw new Error('Form submitted incorrectly');
+  }
+  
+  const joke = await db.joke.create({
+    data: { name, content }
+  });
+  return redirect(`/jokes/${joke.id}`); 
+}
+
+
 export default function NewJokeRoute() {
   return (
     <div>
